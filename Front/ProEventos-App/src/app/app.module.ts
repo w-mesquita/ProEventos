c06fa9d9313fda5eb3408aae791c66a592acb0d4
w-mesquita <tom_rast@hotmail.com>
2022-02-10 22:07:11 -0300

    Seção 5: Angular, interpolação, diretiva e binding

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -7,18 +7,29 @@ import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatMenuModule} from '@angular/material/menu';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    MatDividerModule,
+    MatMenuModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,8 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  showImage: boolean = true;
+  private _filtroEventos: string = "";
+
+  public get filtroEventos(): string {
+    return this._filtroEventos;
+  }
+
+  public set filtroEventos(value: string){
+    this._filtroEventos = value;
+    this.eventosFiltrados = this.filtroEventos ? this.filtrarEventos(this.filtroEventos) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+    (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+    evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
+
+  mostrarImagem(){
+    this.showImage = !this.showImage;
+  }
 
   ngOnInit(): void {
     this.getEventos();
@@ -18,7 +44,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): any {
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     )
 
